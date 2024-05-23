6536f356381fcb266a740fa2beffaaff4de04fa8
Bokter <rojasdjuan18@gmail.com>
2024-05-22 19:34:07 -0500

    Setting Models

--- src/core/models/Add.java
+++ src/core/models/Add.java
@@ -1,6 +1,14 @@
 package core.models;
 
 
-public class Add {
+public class Add extends Operation{
 
+    public Add(double number1, double number2, String operator, double result) {
+        super(number1, number2, operator, result);
+    }
+    
+    @Override
+    public double operate(){  
+        return this.number1 + this.number2;
+    }
 }
--- src/core/models/Operation.java
+++ src/core/models/Operation.java
@@ -1,19 +1,25 @@
 package core.models;
 
-public class Operation {
+public abstract class Operation {
     
-    private double number1;
-    private double number2;
-    private String operator;
-    private double result;
+    protected double number1;
+    protected double number2;
+    protected String operator;
+    protected double result;
 
     public Operation(double number1, double number2, String operator, double result) {
         this.number1 = number1;
         this.number2 = number2;
         this.operator = operator;
-        this.result = result;
+        this.result = this.operate();
     }
 
+    public double getResult(){
+        return this.result;
+    }
+    
+    public abstract double operate();
+    
     @Override
     public String toString() {
         return number1 + " " + operator + " " + number2 + " = " + result;
